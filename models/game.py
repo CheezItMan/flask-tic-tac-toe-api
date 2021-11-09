8e44cc8c3f91b561ce2061b62d2d91173440dc22
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-08 19:29:00 -0800

    games model

---
+++ models/game.py
@@ -0,0 +1,21 @@
+from app import db
+
+
+class Game(db.Model):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    winner = db.Column(db.String(16), nullable=True)
+    player_x_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    player_x = db.relationship("Player", foreign_keys="Games.player_x_id")
+    player_o_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    player_o = db.relationship("Player", foreign_keys="Games.player_o_id")
+
+
+def to_dict(self):
+    return {
+        "id": self.id,
+        "winner": self.winner,
+        "player_x": self.player_x.to_dict(),
+        "player_o": self.player_o.to_dict()
+    }
